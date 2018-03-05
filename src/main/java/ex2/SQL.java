240c951dd1ede4f709099e1f17cf29693d9cbe10
edward <edward@hollywood.ian>
2018-03-05 16:09:56 -0500

    Stuff

--- src/main/java/ex2/SQL.java
+++ src/main/java/ex2/SQL.java
@@ -58,6 +58,7 @@ Site	SiteID	INT	Auto Increment PK
 	 */
 	 
 	/*
+	 * Question 1
 	 * CREATE TABLE final_answer AS (
 	 *   SELECT * FROM (SELECT distinct(CookieId) FROM ( ^code above^ ) ) A 
 	 *     INNER JOIN  (SELECT min(ViewTimeStamp), max(ViewTimeStamp), CookieId) B
